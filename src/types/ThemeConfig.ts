90e9c1283456f169e76140c2beaa3f8e69f997fe
Mario De Los Santos <mario.jdls@gmail.com>
2024-02-12 15:27:21 -0500

    feat: updates ThemeConfig to take dataAttribute and use it in withGlobals

--- src/types/ThemeConfig.ts
+++ src/types/ThemeConfig.ts
@@ -2,6 +2,7 @@ import type { Theme } from "./Theme";
 
 export interface ThemeConfig {
   clearable?: boolean;
+  dataAttribute?: string;
   default?: string; // gets added via globalTypes (globals["dataTheme"])
   list: Theme[];
   onChange?: (themeName: Theme | undefined) => void;
