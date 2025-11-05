dee6fade22775f2334d427bd7924725e9c4628bf
Paul Büchner <45827409+paulbuechner@users.noreply.github.com>
2025-11-05 03:45:37 +0100

    feat: update to storybook v10 (#370)
    - see https://storybook.js.org/docs/addons/addon-migration-guide

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,6 @@
 export const ADDON_ID = "storybook/addon-data-theme-switcher";
 export const TOOL_ID = `${ADDON_ID}/tool`;
+export const TAB_ID = `${ADDON_ID}/tab`;
+
 export const DATA_THEME_KEY = "dataTheme";
 export const DATA_THEMES_KEY = "dataThemes";
