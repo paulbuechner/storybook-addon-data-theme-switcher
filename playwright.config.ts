2093adde8feab6c9c9d4f76614655450ea546ff0
Paul Büchner <paul-buechner@outlook.de>
2026-04-04 13:48:22 +0200

    test: reduce `playwright` timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
   testDir: "./e2e",
-  timeout: 30_000,
+  timeout: 10_000,
   retries: 0,
   use: {
     baseURL: "http://localhost:6006",
